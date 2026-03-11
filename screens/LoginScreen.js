dd78dc615e394b414aa56012ee2b44f21ae34f6b
Srilaxman-EU <150543973+Srilaxman-EU@users.noreply.github.com>
2026-03-11 14:47:08 +0530

    Create LoginScreen.js with login functionality

---
+++ screens/LoginScreen.js
@@ -0,0 +1,14 @@
+// LoginScreen.js
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+            {/* Implement login functionality here */}
+        </View>
+    );
+};
+
+export default LoginScreen;
